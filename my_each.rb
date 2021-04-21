3c9ad4c9c59b8028e73e73261d3f87eaa001e4be
Patrick Lynch <pajlynch@gmail.com>
2021-04-21 00:49:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,10 @@ list = ["ab", "cd", "ef"]
 def my_each(array)
   i = 0
   while i < array.length
-# The "while" method will always return a value of => nil"
     yield(array[i])
     i = i + 1
   end
+list
 end
 
 my_each(list) {|i| "So I think #{i} is a cool"}
