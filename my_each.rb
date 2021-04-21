89cbbca6c71f4743adbe0c0fde17563ad333500e
Patrick Lynch <pajlynch@gmail.com>
2021-04-21 00:49:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,15 +10,3 @@ list
 end
 
 my_each(list) {|i| "So I think #{i} is a cool"}
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
